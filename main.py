f0db61ca1bc2bb9b8c54f6db2cbb2c0c65b1a4d8
Prakash-Jugunta <Prakashjugunta132005@gmail.com>
2025-02-24 21:18:57 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def bucket_sort(arr):
+    if len(arr)==0:
+        return arr
+    max_value,min_value=max(arr),min(arr)
+    bucket_range=(max_value-min_value)/len(arr)
+    buckets = [[] for _ in range(len(arr))]
+    for num in arr:
+        index=int((num-min_value)/bucket_range)
+        if index == len(arr):
+            index-=1
+        buckets[index].append(num)
+    sorted_arr=[]
+    for bucket in buckets:
+        sorted_arr.extend(sorted(bucket))
+    return sorted_arr
+
+arr=[45,21,9,32,58,33,47]
+sorted_arr = bucket_sort(arr)
+print("Sorted Array:", sorted_arr)
---
+++ test/hello.py
@@ -0,0 +1,2 @@
+def heap():
+    print("heap")
